35d2651e6ce8bf94408d27b7c2ead4bf5daf9532
abbr <wenfred@gmail.com>
2014-06-22 20:28:42 -0700

    retrieve list of SecurityPackages
    Signed-off-by: abbr <wenfred@gmail.com>

--- src/NodeSSPI.cpp
+++ src/NodeSSPI.cpp
@@ -3,6 +3,39 @@
 using namespace v8;
 using namespace std;
 
+sspi_module_rec sspiModuleInfo = {0,};
+
+void init_module()
+{
+	LPSTR lpDllName = WINNT_SECURITY_DLL;
+	INIT_SECURITY_INTERFACE pInit;
+	SECURITY_STATUS ss = SEC_E_INTERNAL_ERROR;
+
+	if (sspiModuleInfo.lpVersionInformation == NULL) {
+		sspiModuleInfo.supportsSSPI = TRUE;
+		sspiModuleInfo.defaultPackage = DEFAULT_SSPI_PACKAGE;
+		__try {
+			if (!(sspiModuleInfo.securityDLL = LoadLibrary(lpDllName))) {
+				throw "Error Loading Security DLL";
+			}
+
+			if (!(pInit = (INIT_SECURITY_INTERFACE) GetProcAddress(sspiModuleInfo.securityDLL, SECURITY_ENTRYPOINT))) {
+				throw "Error calling GetProcAddress";
+			}
+
+			if (!(sspiModuleInfo.functable = pInit())) {
+				throw "Error getting functable";
+			}
+			ss = sspiModuleInfo.functable->EnumerateSecurityPackages(&sspiModuleInfo.numPackages, &sspiModuleInfo.pkgInfo);
+		} __finally {
+			if (ss != SEC_E_OK) {
+				sspiModuleInfo.supportsSSPI = FALSE;
+			}
+		}
+	}
+
+}
+
 Handle<Value> Method(const Arguments& args) {
 	HandleScope scope;
 	TryCatch trycatch;
@@ -14,6 +47,7 @@ Handle<Value> Method(const Arguments& args) {
 }
 
 void init(Handle<Object> exports) {
+	init_module();
 	exports->Set(String::NewSymbol("authenticate"),
 		FunctionTemplate::New(Method)->GetFunction());
 }
--- src/NodeSSPI.h
+++ src/NodeSSPI.h
@@ -10,3 +10,19 @@
 //#include <winsock2.h>
 #include <sspi.h>
 //#include <security.h>
+
+#define WINNT_SECURITY_DLL "SECURITY.DLL"
+#define DEFAULT_SSPI_PACKAGE "NTLM"
+#define UUID_STRING_LEN 64
+
+typedef struct sspi_module_struct {
+    BOOL supportsSSPI;
+    LPSTR defaultPackage;
+    LPOSVERSIONINFO lpVersionInformation;
+    char userDataKeyString[UUID_STRING_LEN];
+    HMODULE securityDLL;
+    SecurityFunctionTable *functable;
+    ULONG numPackages;
+    PSecPkgInfo pkgInfo;
+} sspi_module_rec;
+
