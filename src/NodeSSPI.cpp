cfdfd4dc276adc8f7da007daa6b0a406cc16410b
abbr <wenfred@gmail.com>
2014-07-04 13:25:47 -0700

    Implemented cleanup_sspi_connection. Still need to hook to connection close event to perform complete cleanup.
    Signed-off-by: abbr <wenfred@gmail.com>

--- src/NodeSSPI.cpp
+++ src/NodeSSPI.cpp
@@ -38,7 +38,7 @@ void init_module()
 	}
 }
 
-void note_sspi_auth_failure(Local<Object> opts,Local<Object> req,Local<Object> res){
+void note_sspi_auth_failure(const Local<Object> opts,const Local<Object> req,Local<Object> res){
 	int nWays = 0;
 	int nSSPIPkgs = 0;
 	bool offerBasic = false, offerSSPI = false;
@@ -71,6 +71,22 @@ void note_sspi_auth_failure(Local<Object> opts,Local<Object> req,Local<Object> r
 	res->Get(String::New("setHeader"))->ToObject()->CallAsFunction(res, 2, argv);
 	res->Set(String::New("statusCode"),Integer::New(401));
 }
+
+void cleanup_sspi_connection(Local<Object> conn)
+{
+	sspi_connection_rec *pSCR = 0;
+	PCtxtHandle outPch = 0;
+	if (conn->HasOwnProperty(String::New("svrCtx"))){
+		Local<External> wrap = Local<External>::Cast(conn->Get(String::New("svrCtx"))->ToObject()->GetInternalField(0));
+		pSCR = static_cast<sspi_connection_rec *>(wrap->Value());
+		outPch = &pSCR->server_context;
+		sspiModuleInfo.functable->DeleteSecurityContext(outPch);
+		outPch->dwLower = outPch->dwUpper = 0;
+		free(pSCR);
+		conn->Delete(String::New("svrCtx"));
+	}
+}
+
 /*
 * args[0]: opts
 * args[1]: req
@@ -202,55 +218,42 @@ Handle<Value> Authenticate(const Arguments& args) {
 		switch (ss) {
 		case SEC_I_COMPLETE_NEEDED:
 		case SEC_I_CONTINUE_NEEDED:
-		case SEC_I_COMPLETE_AND_CONTINUE: {
-			CStringA base64;
-			int base64Length = Base64EncodeGetRequiredLength(outbuf.cbBuffer);
-			Base64Encode(pOutBuf.get(),
-				outbuf.cbBuffer,
-				base64.GetBufferSetLength(base64Length),
-				&base64Length, ATL_BASE64_FLAG_NOCRLF);
-			base64.ReleaseBufferSetLength(base64Length);
-			std::string authHStr = schema + " " + std::string(base64.GetString());
-			Handle<Value> argv[] = { String::New("WWW-Authenticate"), String::New(authHStr.c_str()) };
-			res->Get(String::New("setHeader"))->ToObject()->CallAsFunction(res, 2, argv);
-			res->Set(String::New("statusCode"), Integer::New(401));
-			break;
-										  }
+		case SEC_I_COMPLETE_AND_CONTINUE: 
+			{
+				CStringA base64;
+				int base64Length = Base64EncodeGetRequiredLength(outbuf.cbBuffer);
+				Base64Encode(pOutBuf.get(),
+					outbuf.cbBuffer,
+					base64.GetBufferSetLength(base64Length),
+					&base64Length, ATL_BASE64_FLAG_NOCRLF);
+				base64.ReleaseBufferSetLength(base64Length);
+				std::string authHStr = schema + " " + std::string(base64.GetString());
+				Handle<Value> argv[] = { String::New("WWW-Authenticate"), String::New(authHStr.c_str()) };
+				res->Get(String::New("setHeader"))->ToObject()->CallAsFunction(res, 2, argv);
+				res->Set(String::New("statusCode"), Integer::New(401));
+				break;
+			}
 		case SEC_E_INVALID_TOKEN:
-			//log_sspi_invalid_token(ctx->r, &ctx->hdr, APR_FROM_OS_ERROR(GetLastError()));
-			//ctx->scr->sspi_failing = 1;
-			//ctx->scr->package = 0;
-			//note_sspi_auth_failure(ctx->r);
-			//cleanup_sspi_connection(ctx->scr);
-			//return HTTP_UNAUTHORIZED;
-			note_sspi_auth_failure(opts,req,res);
-			res->Set(String::New("statusCode"), Integer::New(401));
-			break;
-
 		case SEC_E_LOGON_DENIED:
-			//log_sspi_logon_denied(ctx->r, &ctx->hdr, APR_FROM_OS_ERROR(GetLastError()));
-			//ctx->scr->sspi_failing = 1;
-			//ctx->scr->package = 0;
-			//note_sspi_auth_failure(ctx->r);
-			//cleanup_sspi_connection(ctx->scr);
-			//return HTTP_UNAUTHORIZED;
-			res->Set(String::New("statusCode"), Integer::New(401));
-			break;
-
+			{
+				note_sspi_auth_failure(opts,req,res);
+				cleanup_sspi_connection(conn);
+				res->Set(String::New("statusCode"), Integer::New(401));
+				break;
+			}
 		case SEC_E_INVALID_HANDLE:
 		case SEC_E_INTERNAL_ERROR:
 		case SEC_E_NO_AUTHENTICATING_AUTHORITY:
 		case SEC_E_INSUFFICIENT_MEMORY:
-			//ap_log_rerror(APLOG_MARK, APLOG_ERR, APR_FROM_OS_ERROR(GetLastError()), ctx->r,
-			//	"access to %s failed, reason: cannot generate server context", ctx->r->uri);
-			//return HTTP_INTERNAL_SERVER_ERROR;
-			res->Set(String::New("statusCode"), Integer::New(500));
-			break;
+			{
+				cleanup_sspi_connection(conn);
+				res->Set(String::New("statusCode"), Integer::New(500));
+				break;
+			}
 		case SEC_E_OK:
 			//return OK;
 			break;
 		}
-		req->Set(String::New("user"), String::New("Fred"));
 	}
 	catch (std::exception& ex){
 		args[2]->ToObject()->Set(String::New("statusCode"), Integer::New(500));
