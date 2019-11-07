df31bf20953c1bc28d6715994ea22c0b49aee930
Keith Leung <keith.leung@futurefacilities.com>
2019-11-07 22:06:32 +0800

    Napi porting

--- binding.gyp
+++ binding.gyp
@@ -2,6 +2,15 @@
   "targets": [
     {
       "target_name": "NodeSSPI",
+      "cflags!": [ "-fno-exceptions" ],
+      "cflags_cc!": [ "-fno-exceptions" ],
+      "xcode_settings": { "GCC_ENABLE_CPP_EXCEPTIONS": "YES",
+        "CLANG_CXX_LIBRARY": "libc++",
+        "MACOSX_DEPLOYMENT_TARGET": "10.7",
+      },
+      "msvs_settings": {
+        "VCCLCompilerTool": { "ExceptionHandling": 1 },
+      },
       "sources": [
         "src/*.h",
         "src/*.cpp",
@@ -20,7 +29,10 @@
         }
       },
       "include_dirs" : [
-        "<!(node -e \"require('nan')\")"
+        "<!@(node -p \"require('node-addon-api').include\")"
+      ],
+      "dependencies": [
+        "<!(node -p \"require('node-addon-api').gyp\")"
       ]
     }
   ]
--- src/NodeSSPI.h
+++ src/NodeSSPI.h
@@ -1,9 +1,7 @@
 #pragma once
 
 #define SECURITY_WIN32 1
-#include <node.h>
-#include <nan.h>
-#include <v8.h>
+#include <napi.h>
 
 /* System headers */
 #include <string>
@@ -44,3 +42,77 @@ typedef struct {
     TimeStamp server_ctxtexpiry;
 } sspi_connection_rec;
 
+class NodeSSPIException : public std::exception {
+public:
+	NodeSSPIException(const char * pMsg, const UINT http_code = 500)
+		: std::exception(pMsg), http_code(http_code) {
+	}
+	UINT http_code;
+};
+
+class Baton {
+public:
+	Baton() {
+		err = 0;
+		pGroups = 0;
+		basicDomain = 0;
+		pInToken = 0;
+		pSCR = 0;
+		isTesting = false;
+	}
+	~Baton() {
+		if (!callback.IsEmpty())	callback.Reset();
+		if (!req.IsEmpty()) req.Reset();
+		if (!res.IsEmpty()) res.Reset();
+		if (!conn.IsEmpty()) conn.Reset();
+		if (!opts.IsEmpty()) opts.Reset();
+		if (pGroups) delete pGroups;
+		if (err) delete err;
+		if (basicDomain) delete basicDomain;
+		if (pInToken) free(pInToken);
+	}
+	Napi::FunctionReference callback;
+	//int error_code;
+	//std::string error_message;
+
+	// Custom data
+	Napi::ObjectReference req;
+    Napi::ObjectReference res;
+    Napi::ObjectReference conn;
+    Napi::ObjectReference opts;
+	std::string sspiPkg;
+	ULONG ss;
+	std::string user;
+	std::string userSid;
+	bool retrieveGroups;
+	NodeSSPIException * err;
+	std::vector<std::string> *pGroups;
+	BYTE *pInToken;
+	ULONG pInTokenSz;
+	sspi_connection_rec *pSCR;
+	std::string *basicDomain;
+	// are we running module installation testing?
+	bool isTesting;
+};
+
+class AsyncBasicWorker : public Napi::AsyncWorker {
+ public:
+  AsyncBasicWorker(Napi::Function& cb, Baton* baton);
+  ~AsyncBasicWorker();
+  void Execute ();
+  void OnOK();
+  void OnError();
+ private:
+  Baton* pBaton;
+};
+
+class AsyncSSPIWorker : public Napi::AsyncWorker {
+ public:
+  AsyncSSPIWorker(Napi::Function& cb, Baton* baton);
+  ~AsyncSSPIWorker();
+  void Execute ();
+  void OnOK();
+  void OnError();
+ private:
+  Baton* pBaton;
+};
